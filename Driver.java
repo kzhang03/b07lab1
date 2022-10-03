0f1b28781ae169ae8c1d5d7d50b721966211d972
Kerry Zhang <ker.zhang@mail.utoronto.ca>
2022-10-02 23:40:06 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,34 @@
-public class Driver { 
- public static void main(String [] args) { 
-  Polynomial p = new Polynomial(); 
-  System.out.println(p.evaluate(3)); 
-  double [] c1 = {6,0,0,5}; 
-  Polynomial p1 = new Polynomial(c1); 
-  double [] c2 = {0,-2,0,0,-9}; 
-  Polynomial p2 = new Polynomial(c2); 
-  Polynomial s = p1.add(p2); 
-  System.out.println("s(0.1) = " + s.evaluate(0.1)); 
-  if(s.hasRoot(1)) 
-   System.out.println("1 is a root of s"); 
-  else 
-   System.out.println("1 is not a root of s"); 
- } 
-} 
+public class Driver {
+    public static void main(String [] args) { 
+        Polynomial p = new Polynomial(); 
+        System.out.println(Arrays.toString(p.function)); 
+        System.out.println(Arrays.toString(p.exponent)); 
+
+        double [] c1 = new double[]{6,5};
+        int[] e1 = {0,3}; 
+        Polynomial p1 = new Polynomial(c1, e1);
+
+        double [] c2 = {-2,-9}; 
+        int[] e2 = {1,4};
+        Polynomial p2 = new Polynomial(c2, e2); 
+
+        Polynomial s = p1.add(p2); 
+
+        System.out.println("s(0.1) = " + s.evaluate(0.1)); 
+        if(s.hasRoot(1)) 
+            System.out.println("1 is a root of s"); 
+        else 
+            System.out.println("1 is not a root of s"); 
+        
+        Polynomial m = p1.multiply(p2);
+        System.out.println(Arrays.toString(m.function)); 
+        System.out.println(Arrays.toString(m.exponent)); 
+
+        m.saveToFile("multiply.txt");
+
+        File file = new File("multiply.txt");
+        Polynomial f = new Polynomial(file);
+        System.out.println(Arrays.toString(f.function)); 
+        System.out.println(Arrays.toString(f.exponent)); 
+    }
+}
--- Polynomial.java
+++ Polynomial.java
@@ -1,34 +1,183 @@
+import java.util.Arrays;
+import java.io.File;
+import java.io.*;
+
 public class Polynomial {
 
     double [] function;
+    int [] exponent;
 
     public Polynomial() {
-        function = new double[1];
-        function[0] = 0;
+        function = null;
+        exponent = null;
     }
 
-    public Polynomial(double[] co) {
+    public Polynomial(double[] co, int[] exp){
+        this.function = co;
+        this.exponent = exp;
+    }
 
-        function = new double[co.length];
-        for (int i = 0; i < co.length; i++)
-            function[i] = co[i];
+    public boolean present(String str, char key) {
+        for (int i = 0; i < str.length(); i++)
+            if (str.charAt(i) == key)
+                return true;
+        return false;
+    }
+
+    public Polynomial(File f) {
+        try{
+            BufferedReader contents = new BufferedReader(new FileReader(f));
+            String curr = contents.readLine();
+            String[] arr;
+
+            int temp = 1;
+            if (curr.charAt(0) == '-') {
+                arr = curr.substring(1).split("-|\\+");
+                temp = -1;
+            }
+            else
+                arr = curr.split("-|\\+");
+            
+            int length = arr.length;
+            double[] new_co = new double[length];
+            int[] new_exp = new int[length];
+
+            for (int i = 0; i < length; i++) {
+                if (!present(arr[i], 'x')) {
+                    new_co[i] = temp*Double.parseDouble(arr[i]);
+                    new_exp[i] = 0;
+                }
+                else {
+                    int n;
+                    for (n = 0; n < arr[i].length(); n++) 
+                        if (arr[i].charAt(n) == 'x')
+                            break;
+                    new_co[i] = temp*Double.parseDouble(arr[i].substring(0, n));
+                    new_exp[i] = Integer.parseInt(arr[i].substring(n + 1, arr[i].length()));
+
+                    if (i != 0 && curr.charAt(curr.indexOf(arr[i].substring(0, n)) - 1) == '-')
+                        new_co[i] = new_co[i]*-1;
+                }
+                temp = 1;
+            }
+
+            this.function = new_co;
+            this.exponent = new_exp;
+        }
+        catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public double[] flip_co(double[] co) {
+        var j = co.length;
+        double[] b = new double[j];
+        for (var i : co) {
+            b[--j] = i;
+        }
+        return b;
+    }
+
+    public int[] flip_exp(int[] exp) {
+        var j = exp.length;
+        int[] b = new int[j];
+        for (var i : exp) {
+            b[--j] = i;
+        }
+        return b;
     }
 
     public Polynomial add(Polynomial p) {
 
-        int temp;
-        Polynomial arr;
-        if (p.function.length > this.function.length) {
-            temp = this.function.length;
-            arr = new Polynomial(p.function);
+        int[] new_arr;
+        double[] coefficients;
+        int length = 0;
+
+        double[] temp_co1;
+        int[] temp_exp1;
+        double[] temp_co2;
+        int[] temp_exp2;
+
+        if (this.exponent[this.exponent.length - 1] < this.exponent[0]) {
+            temp_co1 = flip_co(this.function);
+            temp_exp1 = flip_exp(this.exponent);
         }
         else {
-            temp = p.function.length;
-            arr = new Polynomial(this.function);
+            temp_co1 = this.function;
+            temp_exp1 = this.exponent;
         }
 
-        for (int i = 0; i < temp; i++)
-            arr.function[i] = p.function[i] + this.function[i];
+        if (p.exponent[p.exponent.length - 1] < p.exponent[0]) {
+            temp_co2 = flip_co(p.function);
+            temp_exp2 = flip_exp(p.exponent);
+        }
+        else {
+            temp_co2 = p.function;
+            temp_exp2 = p.exponent;
+        }
+
+        int[] longer;
+        int[] shorter;
+
+        if (temp_exp1[temp_exp1.length - 1] > temp_exp2[temp_exp2.length - 1]) {
+            longer = new int[temp_exp1.length];
+            for (int i = 0; i < temp_exp1.length; i++)
+                longer[i] = temp_exp1[i];
+                            
+            shorter = new int[temp_exp2.length];
+            for (int i = 0; i < temp_exp2.length; i++)
+                shorter[i] = temp_exp2[i];
+        }
+        else {
+            longer = new int[temp_exp2.length];
+            for (int i = 0; i < temp_exp2.length; i++)
+                longer[i] = temp_exp2[i];
+            
+            shorter = new int[temp_exp1.length];
+            for (int i = 0; i < temp_exp1.length; i++)
+                shorter[i] = temp_exp1[i];
+        }
+
+        length = longer.length + shorter.length; 
+        for (int i = 0; i < longer.length; i++) {
+            for (int j = 0; j < shorter.length; j++) {
+                if (shorter[j] == longer[i]) {
+                    length--;
+                    break;
+                }
+            }
+        }
+
+        new_arr = new int[length];
+
+        int curr = longer.length;
+        for (int i = 0; i < longer.length; i++)
+            new_arr[i] = longer[i];
+
+        for (int i = 0; i < shorter.length; i++) {
+            if (Arrays.binarySearch(longer, shorter[i]) <= -1){
+                new_arr[curr] = shorter[i];
+                curr++;
+            }
+        }
+
+        Arrays.sort(new_arr);
+
+        coefficients = new double[length];
+        int pos;
+
+        for (int i = 0; i < temp_co1.length; i++) {
+             pos = Arrays.binarySearch(new_arr, temp_exp1[i]);
+             coefficients[pos] += temp_co1[i];
+        }
+        
+        for (int i = 0; i < temp_co2.length; i++) {
+             pos = Arrays.binarySearch(new_arr, temp_exp2[i]);
+             coefficients[pos] += temp_co2[i];
+        }
+
+        Polynomial arr = new Polynomial(coefficients, new_arr);
 
         return arr;
     }
@@ -38,18 +187,108 @@ public class Polynomial {
         double soln = 0;
 
         for (int i = 0; i < this.function.length; i++)
-            soln += Math.pow(sub, i)*this.function[i];
+            soln += Math.pow(sub, this.exponent[i])*this.function[i];
 
         return soln;
     }
 
     public boolean hasRoot(double root) {
+        return this.evaluate(root) == 0;
+    }
 
-        double soln = this.evaluate(root);
+    public Polynomial multiply(Polynomial p) {
 
-        if (soln <= 0.01 && soln >= -0.01)
-            return true;
-        else
-            return false;
+        double[] temp_co1;
+        int[] temp_exp1;
+        double[] temp_co2;
+        int[] temp_exp2;
+    
+        if (this.exponent[this.exponent.length - 1] < this.exponent[0]) {
+            temp_co1 = flip_co(this.function);
+            temp_exp1 = flip_exp(this.exponent);
+        }
+        else {
+            temp_co1 = this.function;
+            temp_exp1 = this.exponent;
+        }
+    
+        if (p.exponent[p.exponent.length - 1] < p.exponent[0]) {
+            temp_co2 = flip_co(p.function);
+            temp_exp2 = flip_exp(p.exponent);
+        }
+        else {
+            temp_co2 = p.function;
+            temp_exp2 = p.exponent;
+        }
+
+        double[] length_co1 = new double[temp_exp1[temp_exp1.length - 1] + 1];
+        double[] length_co2 = new double[temp_exp2[temp_exp2.length - 1] + 1];
+
+        for (int i = 0; i < temp_co1.length; i++) {
+            length_co1[temp_exp1[i]] = temp_co1[i];
+        }
+        for (int j = 0; j < temp_co2.length; j++) {
+            length_co2[temp_exp2[j]] = temp_co2[j];
+        }
+
+        int count = 0;
+        double[] m_co = new double[length_co1.length + length_co2.length - 1];
+
+        for (int i = 0; i < length_co1.length; i++) {
+            for (int j = 0; j < length_co2.length; j++) {
+                m_co[i + j] += length_co1[i]*length_co2[j];
+            }
+        }
+        for (int j = 0; j < m_co.length; j++) {
+            if (m_co[j] != 0) {
+                count++;
+            }
+        }
+
+        int pos = 0;
+        double[] final_co = new double[count];
+        int[] final_exp = new int[count];
+
+        for (int i = 0; i < m_co.length; i++) {
+            if (m_co[i] != 0) {
+                final_co[pos] = m_co[i];
+                final_exp[pos] = i;
+                pos++;
+            }
+        }
+    
+        Polynomial arr = new Polynomial(final_co, final_exp);
+    
+        return arr;
+        }
+
+    public void saveToFile(String s) {
+        try {
+            File f = new File(s);
+            PrintWriter pw;
+            if (f.exists()) {
+                pw = new PrintWriter(f);
+            }
+            else {
+                pw = new PrintWriter(f, "UTF-8");
+            }
+
+            pw.print(""); // Empties text file?
+
+            for (int i = 0; i < this.exponent.length; i++) {
+                if (this.function[i] >= 0 && this.exponent[i] != 0) {
+                    pw.write("+");
+                }
+                pw.write(Double.toString(this.function[i]));
+                if (this.exponent[i] != 0) {
+                    pw.write("x" + Integer.toString(this.exponent[i]));
+                }
+            }
+            pw.close();
+        }
+        catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
